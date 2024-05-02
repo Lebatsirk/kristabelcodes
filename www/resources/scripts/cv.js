69bd0d068a92a39eeea1c7dc5bf0acd338e30716
Kristabel <86485768+Lebatsirk@users.noreply.github.com>
2024-05-02 14:26:09 +0100

    Add tooltip to each CV heading dynamically

--- www/resources/scripts/cv.js
+++ www/resources/scripts/cv.js
@@ -1,3 +1,15 @@
+const objElements = {
+  "#cv_contactdetails_heading": "#cv_contactdetails_content",
+  "#cv_personalstatement_heading": "#cv_personalstatement_content",
+  "#cv_skills_heading": "#cv_skills_content",
+  "#cv_education_heading": "#cv_education_content",
+  "#cv_volunteerexperience_heading": "#cv_volunteerexperience_content",
+  "#cv_workexperience_heading": "#cv_workexperience_content",
+  "#cv_interests_heading": "#cv_interests_content",
+};
+
+let elements = Object.keys(objElements);
+
 // If an element is displayed, hide it, otherwise show it
 function toggleFunction(event) {
   let elementId = event.data.elementToToggle;
@@ -5,28 +17,52 @@ function toggleFunction(event) {
   $(elementId).toggle();
 }
 
+function setTooltip(strElement) {
+  $(strElement).attr("title", "Click here to show/hide section contents");
+}
+
+// function setListener(heading, content) {
+//   console.log("heading is " + heading + " and content is " + content);
+//   // $(heading).on("click", { elementToToggle: content }, toggleFunction);
+//   $(heading).css("backgroundColor", "palegreen");
+
+//   $(heading).on(
+//     //"#cv_contactdetails_heading"
+//     "click",
+//     { elementToToggle: content }, //"#cv_contactdetails_content"
+//     toggleFunction
+//   );
+// }
+
 $(document).ready(function () {
-  //   **************************************************************************
-  // The plan here (test into provided) was to set up an object with key:value pairs, then iterate over that to build the toggle functionality using those keys and values as arguments for the headings and content IDBObjectStore, but unfortunately it doesn't apppear to work.  I want to look further into this to see if it's an issue with *when* the elements are created and eventListeners added but for now I am putting in individual eventListeners.
+  // **************************************************************************
+  // The plan here was to set up a JavaScript object with key:value pairs, then
+  // iterate over that to build the toggle functionality using those keys and
+  // values as arguments for the headings and content, but unfortunately it
+  // doesn't apppear to work.  I want to look further into this to see if it's
+  // an issue with *when* the elements are created and eventListeners added but
+  // for now I am putting in individual eventListeners.
 
-  // Just thinking that there might be a way to use the DOM tree to access the correct elements but only if the nesting etc is correct which might require a re-working of the page
-  //   **************************************************************************
-  const objElements = {
-    "#cv_contactdetails_heading": "#cv_contactdetails_content",
-    "#cv_personalstatement_heading": "#cv_personalstatement_content",
-    "#cv_skills_heading": "#cv_skills_content",
-  };
+  // Just thinking that there might be a way to use the DOM tree to access the
+  // correct elements but only if the nesting etc is correct which might require
+  // a re-working of the page.
 
-  let elements = Object.keys(objElements);
-
+  // In good news, I was able to take advantage of the object and iteration to
+  // add a tooltip to each heading even though I cannot (yet?) add the event
+  // listener in the same way.
+  // **************************************************************************
   elements.forEach((element) => {
-    console.log(`Key: ${objElements[element]} \nValue: ${element}`);
+    console.log(`Key: ${element} \nValue: ${objElements[element]}`);
 
-    $(objElements[element]).on(
-      "click",
-      { elementToToggle: element },
-      toggleFunction
-    );
+    setTooltip(element);
+
+    // setListener(element, objElements[element]);
+
+    // $(objElements[element]).on(
+    //   "click",
+    //   { elementToToggle: element },
+    //   toggleFunction
+    // );
   });
   //   **************************************************************************
 
